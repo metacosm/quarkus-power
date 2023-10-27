6596db76137577e69b6346f2699957aa9ae2f6b2
Chris Laprun <claprun@redhat.com>
2023-10-27 19:03:21 +0200

    fix: extend QuarkusCommand to fix help generation

--- deployment/src/main/java/io/quarkiverse/power/deployment/devui/commands/StopCommand.java
+++ deployment/src/main/java/io/quarkiverse/power/deployment/devui/commands/StopCommand.java
@@ -1,22 +1,23 @@
 package io.quarkiverse.power.deployment.devui.commands;
 
-import org.aesh.command.Command;
 import org.aesh.command.CommandDefinition;
+import org.aesh.command.CommandException;
 import org.aesh.command.CommandResult;
 import org.aesh.command.invocation.CommandInvocation;
 
 import io.quarkiverse.power.runtime.PowerSensor;
 import io.quarkiverse.power.runtime.sensors.macos.powermetrics.MacOSPowermetricsSensor;
+import io.quarkus.deployment.console.QuarkusCommand;
 
-@CommandDefinition(name = "stop", description = "Stops power measurement and outputs accumulated power since measures were started", generateHelp = true)
+@CommandDefinition(name = "stop", description = "Stops power measurement and outputs accumulated power since measures were started")
 @SuppressWarnings("rawtypes")
-public class StopCommand implements Command {
+public class StopCommand extends QuarkusCommand {
 
     //    @Inject
     PowerSensor sensor = MacOSPowermetricsSensor.instance;
 
     @Override
-    public CommandResult execute(CommandInvocation commandInvocation) {
+    public CommandResult doExecute(CommandInvocation commandInvocation) throws CommandException, InterruptedException {
         sensor.stop();
         sensor.outputConsumptionSinceStarted(commandInvocation::println);
 
