0a7abfafe6374b972f38652e16e44c0b6a66b380
Chris Laprun <claprun@redhat.com>
2023-11-09 21:44:43 +0100

    feat: improve reporting, adding average and difference to baseline

--- runtime/src/main/java/io/quarkiverse/power/runtime/PowerMeasure.java
+++ runtime/src/main/java/io/quarkiverse/power/runtime/PowerMeasure.java
@@ -6,4 +6,23 @@ public interface PowerMeasure<M extends SensorMeasure> extends SensorMeasure {
     long duration();
 
     M sensorMeasure();
+
+    default double average() {
+        return total() / numberOfSamples();
+    }
+
+    static String asString(PowerMeasure<?> measure) {
+        final var durationInSeconds = measure.duration() / 1000;
+        final var samples = measure.numberOfSamples();
+        final var measuredMilliWatts = measure.total();
+        return String.format("%s / avg: %s (%ds, %s samples)", readableWithUnit(measuredMilliWatts),
+                readableWithUnit(measure.average()), durationInSeconds,
+                samples);
+    }
+
+    static String readableWithUnit(double milliWatts) {
+        String unit = milliWatts >= 1000 ? "W" : "mW";
+        double power = milliWatts >= 1000 ? milliWatts / 1000 : milliWatts;
+        return String.format("%.3f%s", power, unit);
+    }
 }
