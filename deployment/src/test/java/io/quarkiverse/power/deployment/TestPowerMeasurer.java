833f9c48f244d66f6ae7adb3721d4234f4534777
Chris Laprun <claprun@redhat.com>
2024-02-01 13:41:55 +0100

    wip: attempt to test power-server client

---
+++ deployment/src/test/java/io/quarkiverse/power/deployment/TestPowerMeasurer.java
@@ -0,0 +1,8 @@
+package io.quarkiverse.power.deployment;
+
+import io.quarkus.test.Mock;
+import net.laprun.sustainability.power.PowerMeasurer;
+
+@Mock
+public class TestPowerMeasurer extends PowerMeasurer {
+}
