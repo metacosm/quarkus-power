7ab499300b9b409ac93259a1bec1c74dea6a950f
Chris Laprun <claprun@redhat.com>
2024-07-11 22:10:42 +0200

    feat: improve error handling, simplify

--- runtime/src/main/java/net/laprun/sustainability/power/quarkus/runtime/PowerMeasurer.java
+++ runtime/src/main/java/net/laprun/sustainability/power/quarkus/runtime/PowerMeasurer.java
@@ -1,11 +1,9 @@
 package net.laprun.sustainability.power.quarkus.runtime;
 
 import java.lang.management.ManagementFactory;
-import java.util.Optional;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 import com.sun.management.OperatingSystemMXBean;
@@ -25,9 +23,6 @@ public class PowerMeasurer {
 
     private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
     private final Sampler sampler;
-    private Consumer<PowerMeasure> completed;
-    private BiConsumer<Integer, PowerMeasure> sampled;
-    private Consumer<Exception> errorHandler;
     private static PowerMeasurer instance;
 
     public static PowerMeasurer instance() {
@@ -43,7 +38,7 @@ public class PowerMeasurer {
 
     public PowerMeasurer(Sampler sampler) {
         this.sampler = sampler;
-        this.onError(null);
+        this.withErrorHandler(null);
     }
 
     @SuppressWarnings("unused")
@@ -53,23 +48,17 @@ public class PowerMeasurer {
         return (processCpuLoad < 0 || cpuLoad <= 0) ? 0 : processCpuLoad / cpuLoad;
     }
 
-    public void onCompleted(Consumer<PowerMeasure> completed) {
-        this.completed = completed;
+    public PowerMeasurer withCompletedHandler(Consumer<PowerMeasure> completed) {
+        sampler.withCompletedHandler(completed);
+        return this;
     }
 
-    public void onSampled(BiConsumer<Integer, PowerMeasure> sampled) {
-        this.sampled = sampled;
-    }
-
-    public void onError(Consumer<Exception> errorHandler) {
-        this.errorHandler = errorHandler != null ? errorHandler : (exception) -> {
+    public PowerMeasurer withErrorHandler(Consumer<Throwable> errorHandler) {
+        errorHandler = errorHandler != null ? errorHandler : (exception) -> {
             throw new RuntimeException(exception);
         };
-    }
-
-    public Optional<String> additionalSensorInfo() {
-        //        return Optional.ofNullable(measure).flatMap(sensor::additionalInfo);
-        return Optional.empty(); // fixme
+        sampler.withErrorHandler(errorHandler);
+        return this;
     }
 
     public boolean isRunning() {
@@ -77,33 +66,16 @@ public class PowerMeasurer {
     }
 
     public void start(long durationInSeconds, long frequencyInMilliseconds) {
-        try {
-            sampler.start(durationInSeconds, frequencyInMilliseconds);
+        sampler.start(durationInSeconds, frequencyInMilliseconds);
 
-            if (durationInSeconds > 0) {
-                executor.schedule(this::stop, durationInSeconds, TimeUnit.SECONDS);
-            }
-        } catch (Exception e) {
-            handleError(e);
-        }
-    }
-
-    private void handleError(Exception e) {
-        errorHandler.accept(e);
-        try {
-            sampler.stop(completed);
-        } catch (Exception ex) {
-            // ignore shutting down exceptions
+        if (durationInSeconds > 0) {
+            executor.schedule(this::stop, durationInSeconds, TimeUnit.SECONDS);
         }
     }
 
     public void stop() {
-        try {
-            if (isRunning()) {
-                sampler.stop(completed);
-            }
-        } catch (Exception e) {
-            handleError(e);
+        if (isRunning()) {
+            sampler.stop();
         }
     }
 }
--- runtime/src/main/java/net/laprun/sustainability/power/quarkus/runtime/ServerSampler.java
+++ runtime/src/main/java/net/laprun/sustainability/power/quarkus/runtime/ServerSampler.java
@@ -4,6 +4,7 @@ import java.net.ConnectException;
 import java.net.URI;
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 import jakarta.ws.rs.ProcessingException;
@@ -30,6 +31,9 @@ public class ServerSampler implements Sampler {
     private OngoingPowerMeasure measure;
     private SensorMetadata metadata;
     private static final long pid = ProcessHandle.current().pid();
+    private Consumer<PowerMeasure> completed;
+    private Consumer<Throwable> errorHandler;
+    private BiConsumer<Integer, PowerMeasure> sampled;
 
     @ConfigProperty(name = "power-server.url", defaultValue = "http://localhost:20432")
     URI powerServerURI;
@@ -45,7 +49,7 @@ public class ServerSampler implements Sampler {
 
         final var powerForPid = base.path("{pid}").resolveTemplate("pid", pid);
         powerAPI = SseEventSource.target(powerForPid).build();
-        powerAPI.register(this::update, (e) -> System.out.println("Exception: " + e.getMessage()));
+        powerAPI.register(this::onEvent, this::stopOnError, this::onComplete);
     }
 
     public ServerSampler() {
@@ -69,21 +73,33 @@ public class ServerSampler implements Sampler {
             if (e instanceof ProcessingException processingException) {
                 final var cause = processingException.getCause();
                 if (cause instanceof ConnectException connectException) {
-                    throw new RuntimeException(
+                    stopOnError(new RuntimeException(
                             "Couldn't connect to power-server. Please see the instructions to set it up and run it.",
-                            connectException);
+                            connectException));
                 }
             }
-            throw new RuntimeException(e);
+            stopOnError(e);
         }
     }
 
-    private void update(InboundSseEvent event) {
-        final var measureFromServer = event.readData(SensorMeasure.class);
-        update(measureFromServer);
+    @Override
+    public void stopOnError(Throwable e) {
+        errorHandler.accept(e);
+        if(measure != null && measure.numberOfSamples() > 0) {
+           stop();
+        }
     }
 
-    private void update(SensorMeasure measureFromServer) {
+    private void onComplete() {
+        System.out.println("Finished!");
+    }
+
+    private void onEvent(InboundSseEvent event) {
+        final var measureFromServer = event.readData(SensorMeasure.class);
+        record(measureFromServer);
+    }
+
+    private void record(SensorMeasure measureFromServer) {
         if (measureFromServer != null) {
             final var components = measureFromServer.components();
             if (Arrays.equals(new double[] { -1.0 }, components)) {
@@ -98,10 +114,22 @@ public class ServerSampler implements Sampler {
         }
     }
 
-    public void stop(Consumer<PowerMeasure> completed) {
+    public void stop() {
         powerAPI.close();
         final var measured = new StoppedPowerMeasure(measure);
         measure = null;
         completed.accept(measured);
+    }
+
+    @Override
+    public Sampler withCompletedHandler(Consumer<PowerMeasure> completed) {
+        this.completed = completed;
+        return this;
+    }
+
+    @Override
+    public Sampler withErrorHandler(Consumer<Throwable> errorHandler) {
+        this.errorHandler = errorHandler;
+        return this;
     }
 }
