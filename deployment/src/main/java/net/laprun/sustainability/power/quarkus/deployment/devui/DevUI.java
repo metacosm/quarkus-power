e3b380088e697438641736bdbafa1970ced1fe40
Chris Laprun <claprun@redhat.com>
2025-01-14 16:46:11 +0100

    feat: add initial Dev UI support

--- deployment/src/main/java/net/laprun/sustainability/power/quarkus/deployment/devui/DevUI.java
+++ deployment/src/main/java/net/laprun/sustainability/power/quarkus/deployment/devui/DevUI.java
@@ -5,8 +5,12 @@ import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.BuildSteps;
 import io.quarkus.deployment.builditem.ConsoleCommandBuildItem;
+import io.quarkus.devui.spi.JsonRPCProvidersBuildItem;
+import io.quarkus.devui.spi.page.CardPageBuildItem;
+import io.quarkus.devui.spi.page.Page;
 import net.laprun.sustainability.power.quarkus.deployment.PowerMeasurerBuildItem;
 import net.laprun.sustainability.power.quarkus.deployment.devui.commands.PowerCommands;
+import net.laprun.sustainability.power.quarkus.runtime.devui.PowerService;
 
 @BuildSteps(onlyIf = IsDevelopment.class)
 public class DevUI {
@@ -15,5 +19,21 @@ public class DevUI {
     void addConsoleCommands(PowerMeasurerBuildItem powerMeasurerBI, BuildProducer<ConsoleCommandBuildItem> commands) {
         // register dev console commands
         commands.produce(new ConsoleCommandBuildItem(new PowerCommands(powerMeasurerBI.getMeasurer())));
+    }
+
+    @BuildStep
+    public CardPageBuildItem pages(PowerMeasurerBuildItem powerMeasurerBI) {
+        final var measurer = powerMeasurerBI.getMeasurer();
+        CardPageBuildItem card = new CardPageBuildItem();
+        card.addBuildTimeData("info", measurer.sampler().info());
+        card.addPage(Page.webComponentPageBuilder()
+                .icon("font-awesome-solid:info")
+                .componentLink("qwc-power-info.js"));
+        return card;
+    }
+
+    @BuildStep
+    JsonRPCProvidersBuildItem powerMeasurerJSONProvider() {
+        return new JsonRPCProvidersBuildItem(PowerService.class);
     }
 }
