b88087e579ed53122e2538eaaedb1e2d17a9928f
Chris Laprun <claprun@redhat.com>
2025-01-21 15:22:20 +0100

    fix: simplify total measure display using Recorder

---
+++ runtime/src/main/java/net/laprun/sustainability/power/quarkus/runtime/TotalRecorder.java
@@ -0,0 +1,39 @@
+package net.laprun.sustainability.power.quarkus.runtime;
+
+import java.util.Arrays;
+import java.util.stream.DoubleStream;
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
+import net.laprun.sustainability.power.SensorMetadata;
+import net.laprun.sustainability.power.SensorUnit;
+import net.laprun.sustainability.power.analysis.Recorder;
+import net.laprun.sustainability.power.analysis.total.TotalSyntheticComponent;
+
+public class TotalRecorder extends TotalSyntheticComponent implements Recorder {
+    private DescriptiveStatistics statistics = new DescriptiveStatistics();
+
+    public TotalRecorder(SensorMetadata metadata, SensorUnit expectedResultUnit, int... totalComponentIndices) {
+        super(metadata, expectedResultUnit, totalComponentIndices);
+    }
+
+    @Override
+    public DoubleStream measures() {
+        return Arrays.stream(statistics.getValues());
+    }
+
+    @Override
+    public double synthesizeFrom(double[] components, long timestamp) {
+        final var value = super.synthesizeFrom(components, timestamp);
+        statistics.addValue(value);
+        return value;
+    }
+
+    public DescriptiveStatistics statistics() {
+        return statistics;
+    }
+
+    public void reset() {
+        statistics = new DescriptiveStatistics();
+    }
+}
