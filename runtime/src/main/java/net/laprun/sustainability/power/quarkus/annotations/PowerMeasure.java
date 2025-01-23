d3922b532f7bb3ff4c8e3ae2924381015c552cb7
Chris Laprun <claprun@redhat.com>
2025-01-23 09:15:22 +0100

    fix: make PowerMeasure non-binding
    Without this, the interceptor would only match annotations using the
    name that it defines. Adding non-binding make it match regardless of
    what users' specify for name.

--- runtime/src/main/java/net/laprun/sustainability/power/quarkus/annotations/PowerMeasure.java
+++ runtime/src/main/java/net/laprun/sustainability/power/quarkus/annotations/PowerMeasure.java
@@ -1,5 +1,6 @@
 package net.laprun.sustainability.power.quarkus.annotations;
 
+import jakarta.enterprise.util.Nonbinding;
 import jakarta.interceptor.InterceptorBinding;
 
 import java.lang.annotation.ElementType;
@@ -13,5 +14,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE, ElementType.CONSTRUCTOR})
 public @interface PowerMeasure {
+    @Nonbinding
     String name();
 }
